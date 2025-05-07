8a2888f7b5024ea6787be8c7c32ab1e0c853625a
NoDanate <fdor.peshkov@bk.ru>
2025-05-07 21:30:36 +0500

    Uhm like first commit here?

--- NewProject/src/app/to-do-items/to-do-items.component.ts
+++ NewProject/src/app/to-do-items/to-do-items.component.ts
@@ -2,15 +2,14 @@ import {Component, OnInit} from '@angular/core';
 import {ToDoItem} from '../to-do-item';
 import {ITEMS} from '../mock-todo-items';
 import {NgForOf, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-to-do-items',
   imports: [
     NgForOf,
     NgIf,
-    NgSwitch,
-    NgSwitchCase,
-    NgSwitchDefault
+    FormsModule
   ],
   templateUrl: './to-do-items.component.html',
   styleUrl: './to-do-items.component.css',
@@ -22,6 +21,25 @@ export class ToDoItemsComponent implements OnInit {
   OnSelected(item: ToDoItem) {
     this.selectedItem = item
   }
+
+  OnDoubleClick(item: ToDoItem){
+  item.isComplete = !item.isComplete;
+  }
+
+  AddItem(){
+    const newItem:ToDoItem ={
+      id: this.items.length + 1,
+      name: "New Task",
+      isComplete: false
+    };
+
+    this.items.push(newItem);
+  }
+
+  RemoveItem(item: ToDoItem){
+    this.items = this.items.filter(i => i.id !== item.id)
+  }
+
   constructor() {}
   ngOnInit(): void {}
 
