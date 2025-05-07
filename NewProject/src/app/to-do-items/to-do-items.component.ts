3b2c3a9e8f8e6917ce805c165fe0dfe97719312b
IliaKataev <163750267+IliaKataev@users.noreply.github.com>
2025-05-07 21:31:20 +0500

    commit

--- NewProject/src/app/to-do-items/to-do-items.component.ts
+++ NewProject/src/app/to-do-items/to-do-items.component.ts
@@ -1,16 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 import {ToDoItem} from '../to-do-item';
 import {ITEMS} from '../mock-todo-items';
-import {NgForOf, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-to-do-items',
   imports: [
     NgForOf,
     NgIf,
-    NgSwitch,
-    NgSwitchCase,
-    NgSwitchDefault
+    FormsModule
   ],
   templateUrl: './to-do-items.component.html',
   styleUrl: './to-do-items.component.css',
@@ -21,6 +20,27 @@ export class ToDoItemsComponent implements OnInit {
   selectedItem: ToDoItem | undefined;
   OnSelected(item: ToDoItem) {
     this.selectedItem = item
+  }
+
+  OnDoubleClick(item: ToDoItem){
+    item.isComplete = !item.isComplete;
+  }
+
+  AddItem(){
+    const newItem: ToDoItem = {
+      id: this.items.length + 1,
+      name: "New Task",
+      isComplete: false
+    };
+
+    this.items.push(newItem);
+  }
+
+  RemoveItem(item: ToDoItem){
+  this.items = this.items.filter(i => i.id !== item.id)
+    if(this.selectedItem?.id === item.id){
+      this.selectedItem = undefined;
+    }
   }
   constructor() {}
   ngOnInit(): void {}
