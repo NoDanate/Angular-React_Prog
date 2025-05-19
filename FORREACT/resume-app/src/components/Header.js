049fc650a0e06c24337951935baac2fea8fc9a9f
IliaKataev <163750267+IliaKataev@users.noreply.github.com>
2025-05-19 19:26:41 +0500

    commit

---
+++ FORREACT/resume-app/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => (
+    <header className="header">
+        <h1>Kataev Ilia</h1>
+        <p> Cool guy </p>
+    </header>
+)
+
+export default Header;
---
+++ FORREACT/resume-app/src/components/Projects.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Projects = () => (
+    <section className="projects">
+        <h2>Skills</h2>
+        <ul>
+            <li>REACT</li>
+            <li>ANGULAR</li>
+            <li>JS</li>
+            <li>CSS</li>
+        </ul>
+    </section>
+)
+
+export default Projects;
